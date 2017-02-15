af97d9bb6681b276ffe79e2d5bde602f6cccb16f
Claire Leslie <c.leslie@live.ca>
2017-02-15 17:36:13 -0500

    toggleClass (dark / light) (off / on) when button is clicked, change status when button is clicked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-
-
+$(function() {
+  $('.switch>button').click(function() {
+    $('body').toggleClass('dark light');
+    $(this).toggleClass('off on');
+      if ( $(this).hasClass('on') ) {
+        $('.status').text("It's so bright in here");
+      } else {
+        $('.status').text('Hey, who turned off the lights?');
+  }
+});
+});
